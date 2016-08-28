40a88562773eb9aeed3524e9e87945a8ac18b898
Igor Batov <igbatov@gmail.com>
2016-08-28 22:41:02 +0300

    added predictor and densityplot

--- glmtest.R
+++ glmtest.R
@@ -1,4 +1,5 @@
-for(i in c(1:100)){
+d = data.frame(P=rep(NA,100),R=rep(NA,100));
+for(i in c(1:10)){
   for(j in c(1:10)){
     d[(i-1)*10+j,c('P')]=j;
     d[(i-1)*10+j,c('R')]=sample(c("A","B"), size=1, replace=TRUE, prob=c(1/j,(1-1/j)));
@@ -9,3 +10,7 @@ d$R = factor(d$R)
 glmfit = glm(R~P, data=d, family=binomial)
 print(summary(glmfit))
 print(predict(glmfit, newdata = data.frame(P=c(1:10)), type="response"))
+# plot distribution of response (i.e. 'A' and 'B') 
+# among predictor values in a glm 'link' scale 
+# (from https://www.r-bloggers.com/learn-logistic-regression-and-beyond/)
+densityplot(predict(glmfit,type='link'),groups=d$R=='A', auto.key=T)
